ab2ba9b03eaefadba2c1220165abbfcc46da95ef
jappleba <josh.applebaum@flatironschool.com>
2016-04-04 14:40:06 -0400

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+//= require jquery.min.js
+//= require jquery.easing.1.3.min.js
+//= require jquery.form.js
+//= require jquery.validate.min.js
+//= require bootstrap.min.js
+//= require bootstrap-hover-dropdown.min.js
+//= require skrollr.min.js
+//= require skrollr.stylesheets.min.js
+//= require waypoints.min.js
+//= require waypoints-sticky.min.js
+//= require owl.carousel.min.js
+//= require jquery.isotope.min.js
+//= require jquery.easytabs.min.js
+//= require google.maps.api.v3.js
+//= require viewport-units-buggyfill.js
+//= require scripts.js
